72d9f928cefbb4dac81964ab52d2519f054cfd6c
Homa <hmajam83@gmail.com>
2025-08-08 13:00:02 +0330

    created action creators

--- index.js
+++ index.js
@@ -29,5 +29,45 @@ function createStore(){
   };
 };
 
+//app code
+
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+
+
+function addToDo(todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  };
+};
+function removeToDo(id) {
+  return{
+    type:REMOVE_TODO,
+    id,
+  };
+};
+
+function toggleToDo(id) {
+  return{
+    type:TOGGLE_TODO,
+    id,
+  };
+};
+function addGoal(goal) {
+  return{
+    type:ADD_GOAL,
+    goal,
+  };
+};
+function removeGoal(id) {
+  return{
+    type:REMOVE_GOAL,
+    id,
+  };
+};
 
 
