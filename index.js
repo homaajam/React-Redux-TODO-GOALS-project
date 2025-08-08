965853627a079f2bd199fc2c289b71f69af16cb9
Homa <hmajam83@gmail.com>
2025-08-08 12:58:44 +0330

    create the store

---
+++ index.js
@@ -0,0 +1,33 @@
+// library code
+
+function createStore(){
+
+  let state;
+  let listeners=[];
+
+
+  const getState= () => state;
+
+  const subscribe=(listener)=>{
+    listeners.push(listener);
+
+    return ()=>{
+      listeners=listeners.filter((l)=> l !== listener);
+    };
+  };
+
+  const dispatch=(action) => {
+    state= reducer(state,action);
+    listeners.forEach((listener)=> listener());
+
+  };
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  };
+};
+
+
+
