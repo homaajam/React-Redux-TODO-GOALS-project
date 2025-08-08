c6e9aead5d64f3e4e752e5b59e9e7df3e816e2eb
Homa <hmajam83@gmail.com>
2025-08-08 13:05:19 +0330

    add todo reducer function

--- index.js
+++ index.js
@@ -70,4 +70,21 @@ function removeGoal(id) {
   };
 };
 
+function todo(state=[],action){
+  switch(action.type){
+    case  ADD_TODO:
+      return state.concat([action.todo]);
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id);
+    case TOGGLE_TODO:
+      return state.map((todo) =>
+        todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })
+      );
+    default:
+      return state;
+  }
+   
+}
 
